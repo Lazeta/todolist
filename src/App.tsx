90d128ed8ed1bbf2100663be4b3d56d1da1dd72d
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-05-13 19:02:54 +0300

    feat: add data tasks; create type props in component, implement; add map instead of static mot adaptive method.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import TodoList from "./components/TodoList";
+import TodoList, { TaskType } from "./components/TodoList";
 
 function App() {
+  let tasks1: Array<TaskType> = [
+    { id: 1, title: "HTML", isDone: true},
+    { id: 2, title: "CSS", isDone: true},
+    { id: 3, title: "SASS", isDone: true},
+    { id: 4, title: "JS", isDone: true},
+    { id: 5, title: "", isDone: false},
+  ];
+
+  let tasks2: Array<TaskType> = [
+    { id: 1, title: "Webpack", isDone: true },
+    { id: 2, title: "TS", isDone: true},
+    { id: 3, title: "React", isDone: false},
+    { id: 4, title: "Redax", isDone: false},
+    { id: 5, title: "SCSS", isDone: true},
+  ];
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <TodoList />
-      <TodoList />
-      <TodoList />
+      <TodoList title={"What to learn"} tasks={tasks1}/>
+      <TodoList title={"What we done!"} tasks={tasks2}/>
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,38 @@
-export default function TodoList() {
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+type PropsType = {
+  title: string
+  tasks: Array<TaskType>
+}
+
+
+export default function TodoList(props: PropsType) {
     return (
       <div className="font-sans px-5">
-        <h3 className="text-3xl">What to learn</h3>
+        <h3 className="text-3xl">{props.title}</h3>
         <div>
-          <input className='border-solid border-2 border-sky-500 rounded' type="text" />
+          <input className='border-solid border-2 border-sky-500 rounded' type="text" placeholder="text"/>
           <button className='border-solid border-2 border-sky-500 rounded pl-2 pr-2'> + </button>
         </div>
-        <ul className='text-black'>
-          <li><input type="checkbox" checked={true}/><span> HTML</span></li>
-          <li><input type="checkbox" checked={true}/><span> CSS</span></li>
-          <li><input type="checkbox" checked={true}/><span> SASS</span></li>
-          <li><input type="checkbox" checked={false}/><span> React</span></li>
+
+        <ul className='text-black my-3'>
+          {
+            props.tasks.map(task => 
+            <li className=""><input type="checkbox" checked={task.isDone}/>
+              <span> {task.title}</span>
+              <button onClick={() => { alert(task.id) }} className="border-solid border-2 border-sky-200 rounded px-2 mx-3 ">x</button>
+            </li> )
+          }
         </ul>
+
         <div className="space-x-2">
-        <button className="border-solid border-2 border-sky-500 rounded px-2">All </button>
-        <button className="border-solid border-2 border-sky-500 rounded px-2">Active </button>
-        <button className="border-solid border-2 border-sky-500 rounded px-2">Completed </button>
+          <button className="border-solid border-2 border-sky-500 rounded px-2">All </button>
+          <button className="border-solid border-2 border-sky-500 rounded px-2">Active </button>
+          <button className="border-solid border-2 border-sky-500 rounded px-2">Completed </button>
         </div>
       </div>
     );
