0f3888eabdd44fa2be5a6c91f0372c63eb01dd75
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-05-13 12:44:33 +0300

    draw the initial template, draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-
+import React from "react";
 
 function App() {
   return (
-    <div className='text-3x1 font-bold underline'>
-      Hello World!
+    <div className="flex justify-center items-center h-screen">
+      <TodoList />
+      <TodoList />
+      <TodoList />
+    </div>
+  );
+}
+
+function TodoList() {
+  return (
+    <div className="font-sans">
+      <h3 className="text-3xl">What to learn</h3>
+      <div>
+        <input type="text" />
+        <button>+</button>
+      </div>
+      <ul>
+        <li><input type="checkbox" checked={true}/><span> HTML</span></li>
+        <li><input type="checkbox" checked={true}/><span> CSS</span></li>
+        <li><input type="checkbox" checked={true}/><span> SASS</span></li>
+      </ul>
     </div>
   );
 }
