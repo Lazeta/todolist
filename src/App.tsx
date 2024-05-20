06c1b4951ff615c4d17195ffb51ad17c1fab811a
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-05-20 22:44:04 +0300

    feat: add unit test for todolist

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import {
 import { Menu } from "@mui/icons-material"
 
 export type FilterValuesType = "all" | "active" | "completed"
-type TodolistType = {
+export type TodolistType = {
   id: string
   title: string
   filter: FilterValuesType
