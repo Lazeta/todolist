4f03b97f33e01167b79c5853d7ea8b57b560f593
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-05-27 00:34:50 +0300

    remove demo testing

--- src/state/mapArrToStrings/mapArrToStrings.test.js
+++
@@ -1,8 +0,0 @@
-import { mapArrToStrings } from "./mapArrToStrings";
-
-
-describe('mapArrToStrings', () => {
-    test('Корректное значение', () => {
-        expect(mapArrToStrings([1, 2, 3])).toEqual(['1', '2', '3']);
-    })
-})
