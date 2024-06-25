24bba2460dab3d2170d71bbb9625cb8f3e05a5e3
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-06-25 18:36:03 +0300

    last save

--- src/App.tsx
+++ src/App.tsx
@@ -59,9 +59,11 @@ function App() {
   }
 
   function changeFilter(value: FilterValuesType, todolistId: string) {
-    let todolist = todolists.find((tl) => tl.id === todolistId)
-    if (todolist) todolist.filter = value
-    setTodolists([...todolists])
+    setTodolists(todolists.map(filtered => filtered.id === todolistId ? {...filtered, filter: value} : filtered))
+
+    // let todolist = todolists.find((tl) => tl.id === todolistId)
+    // if (todolist) todolist.filter = value
+    // setTodolists([...todolists])
   }
 
   let todolistId1 = v1()
